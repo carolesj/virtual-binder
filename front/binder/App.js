a3861cabb0483254c8ac1fe3f0ed300655e47e76
yancr22 <yanrohwedder@gmail.com>
2019-08-24 13:05:31 -0300

    Created project Binder with Expo-cli

---
+++ front/binder/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+ 
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+   
+  };
+
+ }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
