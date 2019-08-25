81782b2a67fa114b0a4d5ac9ce4960046830eaee
yancr22 <yanrohwedder@gmail.com>
2019-08-25 01:12:35 -0300

    Done RegisterSubject Screen/Add Kanit font

--- front/binder/App.js
+++ front/binder/App.js
@@ -1,25 +1,181 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text, TextInput, Picker, Button, TouchableOpacity } from 'react-native';
+import { AppLoading, Asset, Icon } from 'expo';
+import * as Font from 'expo-font';
+
+
+
 
 export default class App extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      text: '',
+      text2: '',
+      text3: '',
+      isLoadingComplete: false,
+    };
+  } 
 
   render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    );
-   
+      if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+        return (
+          <AppLoading
+            startAsync={this._loadResourcesAsync}
+            onError={this._handleLoadingError}
+            onFinish={this._handleFinishLoading}
+          />
+        );
+      }
+        else{
+          return(
+            <View style={{backgroundColor: '#FAF36C50', flex: 1}}>
+              <Text style = {styles.textHeader}>Adicionar Arquivo</Text>
+              <View style={{flexDirection:"row", justifyContent: 'center'}}>
+                <View>
+                  <Text style={[styles.text,{width: 130}]}>Nome do Professor:</Text>
+                </View>
+                <View>
+                  <TextInput
+                    style={[styles.textInput]}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                  />
+                </View>
+              </View>
+              <View style={{flexDirection:"row", justifyContent: 'center'}}>
+                <View>
+                  <Text style={[styles.text,{width: 130}]}>Nome da Disciplina:</Text>
+                </View>
+                <View>
+                  <TextInput
+                    style={styles.textInput}
+                    onChangeText={(text2) => this.setState({text2})}
+                    value={this.state.text2}
+                  />
+                </View>
+              </View>
+
+              <Text style={[styles.text,{marginLeft: 20}]}>Tipo de recurso:</Text>
+              <Picker
+                selectedValue={this.state.language}
+                style={styles.picker}
+                mode = "dropdown"
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({language: itemValue})
+                }>
+                <Picker.Item label="Listas" color='#F28864' fontFamily = 'Kanit' value="listas" />
+                <Picker.Item label="Provas" color='#F28864'  value="provas" />
+                <Picker.Item label="Material" color='#F28864' value="material" />
+                <Picker.Item label="Outros" color='#F28864'  value="outros" />
+              </Picker>
+              <View style={{flexDirection:"row", justifyContent: 'center', paddingTop: -80, paddingRight: 60}}>
+                <View>
+                  <Text style={[styles.text,{width: 40}]}>Tags:</Text>
+                </View>
+                <View>
+                  <TextInput
+                    style={styles.textInput}
+                    onChangeText={(text3) => this.setState({text3})}
+                    value={this.state.text3}
+                  />
+                </View>
+              </View>
+              <TouchableOpacity style = {styles.button}>
+                <Text style = {{backgroundColor: '#965D5D', color: 'white', fontFamily: 'Kanit', paddingHorizontal: 20}}>
+                  Buscar Arquivos
+                </Text>
+              </TouchableOpacity>
+              <View style={{justifyContent:"flex-end", flexDirection: 'row'}}>
+                <TouchableOpacity style = {[styles.button , {justifyContent: "flex-start"}]}>
+                  <Text style = {{
+                      backgroundColor: 'white', 
+                      color: '#F28864', 
+                      fontFamily: 'Kanit', 
+                      paddingHorizontal: 20, 
+                      borderColor: '#F28864',
+                      borderWidth: 0.5,
+                      padding: 10
+                    }}>
+                    Voltar
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style = {[styles.button , {justifyContent: "flex-end"} ]}>
+                  <Text style = {{
+                      backgroundColor: '#F28864', 
+                      color: 'white', 
+                      fontFamily: 'Kanit', 
+                      paddingHorizontal: 10, 
+                      borderColor: '#F28864',
+                      borderWidth: 0.5,
+                      padding: 10
+                    }}>
+                    Adicionar
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          );
+        }
+  }
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'Kanit': require('./assets/fonts/Kanit/Kanit-Regular.ttf'),
+      }),
+    ]);
   };
 
- }
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
 
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  textInput: {
+      marginTop: 17,
+      height: 20,
+      width: 100,
+      borderBottomColor: '#F28864',
+      fontFamily: 'Kanit',
+      borderBottomWidth: 1,
+      justifyContent:"center",
+      marginRight: 100,
   },
+  textHeader: {
+    paddingTop: 30,
+    fontFamily: 'Kanit',
+    fontSize: 24,
+    color: '#F28864',
+    textAlign: "center", 
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 14,
+    fontFamily: 'Kanit',
+    color: 'brown'
+  },
+  picker: {
+    fontFamily: 'Kanit',
+    color: 'brown',
+    marginLeft: 10,
+    justifyContent: 'center',
+    height: 40, 
+    width: 250,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    width: 250,
+    padding: 10,
+    marginBottom: 20
+  }
 });
